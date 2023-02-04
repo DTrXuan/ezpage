95ebe74868cd10bcfa667822651a50ff8feac046
Truong Xuan Doan <dtruongxuan.cdt@gmail.com>
2023-02-05 03:09:37 +0700

    deploy to firebase

--- public/message.js
+++ public/message.js
@@ -9,55 +9,54 @@ const firebaseConfig = {
   appId: "1:206959232405:web:7c90ca0d831776dbd3e29c"
 };
 
-  firebase.initializeApp(firebaseConfig);
-  
-  const database = firebase.database();
-  const messagesRef = database.ref("messages");
-  
-  const addMessageButton = document.getElementById('add-message-button');
-  const messageInput = document.getElementById('message-input');
-  const userInput = document.getElementById('user-input');
-  const messageList = document.getElementById('message-list');
-  
-  const addMessage = (event) => {
-    event.preventDefault();
-    if (!userInput.value || !messageInput.value) {
-      alert('Username and Message are both required.');
-      return;
-    }
-    const message = {
-      author: userInput.value,
-      text: messageInput.value,
-      date: new Date().toLocaleString(),
-    };
-    messagesRef.push(message);
-    messageInput.value = '';
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+const messagesRef = database.ref("messages");
+
+const addMessageButton = document.getElementById('add-message-button');
+const messageInput = document.getElementById('message-input');
+const userInput = document.getElementById('user-input');
+const messageList = document.getElementById('message-list');
+
+const addMessage = (event) => {
+  event.preventDefault();
+  if (!userInput.value || !messageInput.value) {
+    alert('Username and Message are both required.');
+    return;
+  }
+  const message = {
+    author: userInput.value,
+    text: messageInput.value,
+    date: new Date().toLocaleString(),
   };
-  
-  const renderMessages = () => {
-    messagesRef.on("value", function(snapshot) {
-      messageList.innerHTML = '';
-      snapshot.forEach(function(childSnapshot) {
-        const message = childSnapshot.val();
-        const messageItem = document.createElement('li');
-        messageItem.innerHTML = `
+  messagesRef.push(message);
+  messageInput.value = '';
+};
+
+const renderMessages = () => {
+  messagesRef.on("value", function (snapshot) {
+    messageList.innerHTML = '';
+    snapshot.forEach(function (childSnapshot) {
+      const message = childSnapshot.val();
+      const messageItem = document.createElement('li');
+      messageItem.innerHTML = `
           <p class="date">${message.date} : <strong>${message.text}</strong></p>
         `;
-        const deleteButton = document.createElement('button');
-        deleteButton.innerHTML = 'Delete';
-        deleteButton.onclick = () => {
-          if (message.author === userInput.value || userInput.value === 'xuansama') {
-            childSnapshot.ref.remove();
-          } else {
-            alert('You are not authorized to delete this message.');
-          }
-        };
-        messageItem.appendChild(deleteButton);
-        messageList.appendChild(messageItem);
-      });
+      const deleteButton = document.createElement('button');
+      deleteButton.innerHTML = 'Delete';
+      deleteButton.onclick = () => {
+        if (message.author === userInput.value || userInput.value === 'xuansama') {
+          childSnapshot.ref.remove();
+        } else {
+          alert('You are not authorized to delete this message.');
+        }
+      };
+      messageItem.appendChild(deleteButton);
+      messageList.appendChild(messageItem);
     });
-  };
-  
-  addMessageButton.onclick = addMessage;
-  renderMessages();
-  
+  });
+};
+
+addMessageButton.onclick = addMessage;
+renderMessages();
